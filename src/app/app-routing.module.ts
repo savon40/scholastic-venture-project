88b273f7c40ab317a01aa5fa1d46ef3f55f9e487
Steve Avon <savon40@gmail.com>
2019-08-01 18:40:11 -0400

    sign in component created and form looking good

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: SignInComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAngularModule
+    AmplifyAngularModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [AmplifyService],
   bootstrap: [AppComponent]
