5e8300a0d236c17cc52aa1ea27c17c90a6fde14b
Steve Avon <savon40@gmail.com>
2019-08-06 14:12:11 -0400

    got one chart working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { AuthService } from './components/auth/auth.service';
     AppRoutingModule,
     AmplifyAngularModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ChartsModule
   ],
   providers: [AmplifyService, AuthService],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,6 +18,22 @@ export class HomeComponent implements OnInit {
   ]
   private loginSubscription: Subscription;
 
+  //bar chart
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels = ['Sept', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  // end bar chart
+
+
+
   constructor(
     private authService: AuthService,
     private router: Router
