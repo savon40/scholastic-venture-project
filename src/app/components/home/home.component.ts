589f99d7ca2bc2d1bb1685ec5ce3e3ca504a1f54
Steve Avon <savon40@gmail.com>
2019-08-06 07:48:22 -0400

    before doing the jumbotron thing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { API } from 'aws-amplify';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,6 @@ export class HomeComponent implements OnInit {
     'Steve Avon', 'Cris Bouidel', 'Sean Caesar'
   ]
   private loginSubscription: Subscription;
-
 
   constructor(
     private authService: AuthService,
@@ -30,6 +30,24 @@ export class HomeComponent implements OnInit {
         }
       }
     )
+    // this.getStudents();
+  }
+
+  getStudents() {
+    let myInit = { // OPTIONAL
+      headers: {}, // OPTIONAL
+      response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+      queryStringParameters: {  // OPTIONAL
+      }
+    }
+
+    API.get('ventureAPI', '/students', myInit).then(response => {
+      debugger;
+      // Add your code here
+    }).catch(error => {
+      console.log(error.response);
+      debugger;
+    });
   }
 
 }
