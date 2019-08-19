75fe3dbf9cafb3b0e71b0a6057baed2c9da081c6
Steve Avon <savon40@gmail.com>
2019-08-19 08:57:47 -0400

    good progress on survey response capture

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import * as queries from '../../../graphql/queries';
 import * as custom_queries from '../../../graphql/custom_queries';
+import * as mutations from '../../../graphql/mutations';
 
 @Component({
   selector: 'app-survey',
@@ -25,8 +25,54 @@ export class SurveyComponent implements OnInit {
       console.log('questions', questions);
       this.question_list = questions['data']['listSurveyQuestions']['items'];
       this.isLoading = false;
-      // this.student_list = students['data']['listStudents']['items'];
     });
+  }
+
+  onSubmit(value: any) {
+    // console.log('form submitted');
+    // console.log(value);
+
+    //create survey first
+    /*
+      input CreateSurveyInput {
+        id: ID
+        createdAt: AWSDateTime!
+        surveyStudentId: ID
+
+        - YYYY-MM-DDThh:mm:ss.sssZ format for awsdatetime
+
+        - get student id from a service of some kind
+      }
+    */
+
+    for (const question of this.question_list) {
+      console.log('questionid', question['id']);
+      console.log('answer hopefully', value[question['id']]);
+
+      /*
+        input CreateSurveyResponseInput {
+          id: ID
+          response: String
+          createdAt: AWSDateTime!
+          surveyResponseSurveyId: ID
+          surveyResponseQuestionId: ID
+        }
+
+        const newSR = {
+          response: value[question['id']],
+          createdAt: now?
+          surveyResponseSurveyId: new survey I create here
+          surveyResponseQuestionId: question['id']
+        }
+      */
+
+      // mutations.createSurveyResponse
+    }
+
+
+    // for (const answer of value) {
+    //   console.log('answer', answer);
+    // }
   }
 
 }
