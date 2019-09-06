256227f7c0295eddfab5d797958a82f3605aa702
Steve Avon <savon40@gmail.com>
2019-09-06 17:18:45 -0400

    updating github

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -31,8 +31,8 @@ export class HomeComponent implements OnInit, AfterViewInit {
   public barChartType = 'bar';
   public barChartLegend = true;
   public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Mental Health'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Grades'}
   ];
   // end bar chart
 
